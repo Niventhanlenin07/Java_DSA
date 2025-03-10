322a88e24c64327043c5b15f91b95ab62d030a72
Niventhan Lenin <Niventhan.Lenin@mrcooper.com>
2025-03-10 23:21:42 +0530

    Add file

---
+++ Array.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Array{
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int size=scan.nextInt();
+        int[]arr=new int[size];
+        for (int i=0;i<size;i++){
+            arr[i]=scan.nextInt();
+        }
+        for(int i:arr){
+            System.out.print(i+" ");
+        }
+    }
+}
