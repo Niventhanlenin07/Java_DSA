a8bdd8e4bb1e6899b0cbde5f2dfe7aa146158137
Niventhan Lenin <Niventhan.Lenin@mrcooper.com>
2025-03-21 02:44:41 +0530

    Updated

--- Queue.java
+++ Queue.java
@@ -1,3 +1,35 @@
-public class Queue {
-    
-}
+//Queue using Array
+// class queueArray{
+//     int[]arr=new int[20];
+//     int front=-1,rear=-1;
+//     void enqueue(int val){
+//         if(front==-1){
+//             front=0;
+//         }
+//         arr[++rear]=val;
+//     }
+//     void dequeue(){
+//         System.out.println(arr[front++]);
+//     }
+//     void peek(){
+//         System.out.println(arr[front]);
+//     }
+//     void display(){
+//         for(int i=front;i<=rear;i++){
+//             System.out.print(arr[i]+" ");
+//         }
+//     }
+// }
+// public class Queue {
+//     public static void main(String[] args) {
+//         queueArray q=new queueArray();
+//         q.enqueue(1);
+//         q.enqueue(2);
+//         q.enqueue(3);
+//         q.enqueue(4);
+//         q.peek();
+//         q.dequeue();
+//         q.peek();
+//         q.display();
+//     }
+// }
