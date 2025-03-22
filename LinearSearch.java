12415be3638a2c12c3e9f343d9896b05c8ec9962
Niventhan Lenin <Niventhan.Lenin@mrcooper.com>
2025-03-22 13:41:30 +0530

    Updated

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,3 +1,21 @@
-public class LinearSearch {
-    
-}
+// import java.util.*;
+// public class LinearSearch {
+//     public static void main(String[] args) {
+//         Scanner scan=new Scanner(System.in);
+//         int[]arr={1,2,3,4,5};
+//         int target=scan.nextInt();
+//         int flag=0;
+//         for(int i=0;i<arr.length;i++){
+//             if(arr[i]==target){
+//                 flag=1;
+//             }else{
+//                 flag=0;
+//             }
+//         }
+//         if(flag==1){
+//             System.out.println("Match");
+//         }else{
+//             System.out.println("Not match");
+//         }
+//     }   
+// }
