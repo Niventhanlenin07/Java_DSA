abcc4b86d79ab43283942dca63f9c9465fe69332
Niventhan Lenin <Niventhan.Lenin@mrcooper.com>
2025-03-25 01:56:11 +0530

    Updated

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,5 +1,28 @@
-package Sorting;
+// package Sorting;
 
-public class BubbleSort {
-    
-}
+// import java.util.Arrays;
+
+// public class BubbleSort {
+//     static void sort(int[]arr){
+//         for(int i=1;i<arr.length;i++){
+//             boolean swapped=false;
+//             for(int j=0;j<arr.length-1;j++){
+//                 if(arr[j+1]<arr[j]){
+//                     swapped=true;
+//                     int temp=arr[j];
+//                     arr[j]=arr[j+1];
+//                     arr[j+1]=temp;
+//                 }
+//             }
+//             if(!swapped){
+//                 System.out.println("Correct order in "+i+" step");
+//                 break;
+//             }
+//         }
+//     }
+//     public static void main(String[] args) {
+//         int[]arr={1,2,3,4,5};
+//         sort(arr);
+//         System.out.println(Arrays.toString(arr));
+//     }
+// }
--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -1,5 +1,24 @@
-package Sorting;
+// package Sorting;
 
-public class SelectionSort {
-    
-}
+// import java.util.Arrays;
+
+// public class SelectionSort {
+//     static void sort(int[]arr){
+//         for(int i=0;i<arr.length;i++){
+//             int min_index=i;
+//             for(int j=i+1;j<arr.length;j++){
+//                 if(arr[min_index]>arr[j]){
+//                     min_index=j;
+//                 }
+//             }
+//             int temp=arr[i];
+//             arr[i]=arr[min_index];
+//             arr[min_index]=temp;
+//         }
+//     }
+//    public static void main(String[] args) {
+//       int[]arr={1,4,6,2,3,5};
+//       sort(arr);
+//       System.out.println(Arrays.toString(arr));
+//    } 
+// }
