87c4f39d63bd2c323d0e4c5ecd8e1bd67e33e90d
Niventhan Lenin <Niventhan.Lenin@mrcooper.com>
2025-03-24 21:43:39 +0530

    Updated

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,31 +1,26 @@
 // In binary search the array is ASC or DSEC order only it work.
 // import java.util.*;
 // public class BinarySearch{
-//     static void search(int[]arr,int target){
-//             int mid=arr.length/2;
+//     static int search(int[]arr,int target){
+//         int start=0;
+//         int end=arr.length-1;
+//         while(start<=end){
+//             int mid=start+(end-start)/2;
 //             if(arr[mid]>target){
-//                 if(arr[mid]==arr[0]){
-//                     System.out.println("Not match");
-//                 }else{
-//                     arr=Arrays.copyOfRange(arr,0,mid);
-//                     search(arr, target);
-//                 }
+//                 end=mid-1;
 //             }else if(arr[mid]<target){
-//                 if(arr[mid]==arr[arr.length-1]){
-//                     System.out.println("Not match");
-//                 }else{
-//                     arr=Arrays.copyOfRange(arr,mid+1,arr.length);
-//                     search(arr, target);
-//                 }
+//                 start=mid+1;
 //             }else{
-//                 System.out.println("Match");
+//                 return 1;
 //             }
+//         }
+//         return -1;
 //     }
 //     public static void main(String[] args) {
 //         Scanner scan=new Scanner(System.in);
 //         int[]arr={1,2,3,4,5};
 //         System.out.println("Enter the target");
 //         int target=scan.nextInt();
-//         search(arr,target);
+//         System.out.println(search(arr,target));
 //     }
 // }
