089f57a369018a985ce4d079daec67cd98c6ec6d
Niventhan Lenin <Niventhan.Lenin@mrcooper.com>
2025-05-23 04:20:22 +0530

    Updated

--- HashMap.java
+++ HashMap.java
@@ -1,12 +1,11 @@
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Arrays;
+//import java.util.List;
 
 public class hashmap {
     public static void main(String[] args) {
-        HashMap<String,Integer>h=new HashMap<>();
-        String[]arr={"Niventhan","Prabhu","Abi","Niventhan"};
-        for(String s:arr){
-            h.put(s,h.getOrDefault(s,0)+1);
-        }
-        System.out.println(h.entrySet());
+        ArrayList<String> list = new ArrayList<>();
+        list.addAll(Arrays.asList("Apple", "Banana", "Cherry"));
+        System.out.println(list);
     }
 }
