6eb72bcda8db05dba7112fbee1ec3e273b4400ea
Niventhan Lenin <Niventhan.Lenin@mrcooper.com>
2025-05-23 03:37:34 +0530

    New files

--- HashMap.java
+++ HashMap.java
@@ -1,5 +1,12 @@
-public class HashMap {
+import java.util.HashMap;
+
+public class hashmap {
     public static void main(String[] args) {
-        
+        HashMap<String,Integer>h=new HashMap<>();
+        String[]arr={"Niventhan","Prabhu","Abi","Niventhan"};
+        for(String s:arr){
+            h.put(s,h.getOrDefault(s,0)+1);
+        }
+        System.out.println(h.entrySet());
     }
 }
