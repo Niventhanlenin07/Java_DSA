845a82b2fa1c8f42b925d854078b38428ede3625
Niventhan Lenin <Niventhan.Lenin@mrcooper.com>
2025-05-29 03:53:42 +0530

    Updated

--- hashMap.java
+++ hashMap.java
@@ -1,16 +1,29 @@
-import java.util.HashMap;
-
+import java.util.*;
+class laptop{
+    String Name;
+    String Ram;
+    int price;
+    laptop(String Name,String Ram,int Price){
+        this.Name=Name;
+        this.Ram=Ram;
+        this.price=Price;
+    }
+    public String toString(){
+        return "["+Name+","+Ram+","+price+"]";
+    }
+}
+class check implements Comparator<laptop>{
+    public int compare(laptop l1,laptop l2){
+        return l1.Name.compareTo(l2.Name);
+    }
+} 
 public class hashMap {
     public static void main(String[] args) {
-        int[]arr={1,1,2};
-        HashMap<Integer,Integer>h=new HashMap<>();
-        for(Integer i:arr){
-            h.put(i,h.getOrDefault(i, 0)+1);
-        }
-        for(int i=0;i<arr.length;i++){
-            if(h.get(arr[i])==1){
-                System.out.println(arr[i]);
-            }
-        }
+        List<laptop>l=Arrays.asList(new laptop("HP","12GB", 79999),
+        new laptop("DELL","12GB",49999),
+        new laptop("MAC BOOK","12GB",99999));
+        check c=new check();
+        Collections.sort(l,c);
+        System.out.println(l);
     }
 }
